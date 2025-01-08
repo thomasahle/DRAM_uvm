c6fe4ba8c01f7262a803b9997a785ac19076b7a3
Thomas D Ahle <thomas@ahle.dk>
2025-01-08 18:05:42 +0100

    fix

--- filelist.f
+++ filelist.f
@@ -2,17 +2,17 @@
 +incdir+.
 
 # 1) Compile the packages first (they define classes & types):
-1_dram_pkg.sv
-2_dram_seq_pkg.sv
+dram_pkg.sv
+dram_seq_pkg.sv
 
 # 2) Then compile the UVM components that reference those packages
-3_dram_drv.sv
-4_dram_mon.sv
-5_dram_cov.sv
-6_dram_sb.sv
-7_dram_agent.sv
-8_dram_env.sv
-9_dram_test.sv
+dram_drv.sv
+dram_mon.sv
+dram_cov.sv
+dram_sb.sv
+dram_agent.sv
+dram_env.sv
+dram_test.sv
 
 # 3) Finally, compile your design and top testbench
 design.sv
